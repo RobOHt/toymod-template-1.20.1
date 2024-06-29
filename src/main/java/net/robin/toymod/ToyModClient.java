1b18b284a86542cd58cf594983a027e80de55582
Xiaoyang Zhong <32957875+RobOHt@users.noreply.github.com>
2024-06-29 19:19:01 +0800

    Initial commit

---
+++ src/main/java/net/robin/toymod/ToyModClient.java
@@ -0,0 +1,10 @@
+package net.robin.toymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/robin/toymod/ToyModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.robin.toymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ToyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
